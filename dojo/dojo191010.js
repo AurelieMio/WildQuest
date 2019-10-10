ebf8e571856eeb0293b8547d7271af6db7db4958
Aurelie Miomandre <aureliemiomandre@hotmail.fr>
2019-10-10 11:55:12 +0200

    dojo 2 fini (map et filter)

--- dojo/dojo191010.js
+++ dojo/dojo191010.js
@@ -1,26 +1,45 @@
 // Creation fonction map homemade
-array = [1,2,3] // Tableau d'essai
+array = [1,45,3,74,14,5,67,9,40] // Tableau d'essai
 
-const map = (callback,tableau) => {
-    let array2 = [];
-    for (let i=0; i<tableau.length; i++){
-         array2[i]= callback(tableau[i])
+// const map = (callback,tableau) => {
+//     let array2 = [];
+//     for (let i=0; i<tableau.length; i++){
+//          array2[i]= callback(tableau[i])
+//     }
+//     return array2
+// }
+
+// // 1e version callback
+// const multiply = (valueInit) => {
+//     return valueInit*4;
+// }
+
+// console.log(map(multiply, array))
+
+// // 2e version callback qui marche aussi (la callback renvoie sur une fonction avec des arguments (INCEPTION))
+
+// const multiply = (valueInit, multiplier, diviser) => {
+//     return valueInit*multiplier/diviser;
+// }
+
+// console.log(map((item)=> multiply(item, 4, 12), array))
+
+// Homemade filter (incroyable ca fonctionne \o/)
+
+const filterDIY = (array, cb) => {
+    let array2=[]
+    let h=0
+    for (let j=0; j<array.length; j++){
+        if (cb(array[j])=== true ){
+            array2[h] = array[j]
+            h++
+            console.log(array[j])
+        } else {
+        }
     }
     return array2
 }
 
-// 1e version callback
-const multiply = (valueInit) => {
-    return valueInit*4;
-}
+cb = (item) => item < 20
 
-console.log(map(multiply, array))
-
-// 2e version callback qui marche aussi (la callback renvoie sur une fonction avec des arguments (INCEPTION))
-
-const multiply = (valueInit, multiplier, diviser) => {
-    return valueInit*multiplier/diviser;
-}
-
-console.log(map((item)=> multiply(item, 4, 12), array))
-
+console.log(filterDIY(array, cb))
